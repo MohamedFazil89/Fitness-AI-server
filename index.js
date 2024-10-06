f389cd9f1092238436949a7ed0269bea3badf5da
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-10-06 20:43:40 +0530

    Oauth route add

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
 
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: 'http://localhost:3000' }),
   (req, res) => {
-    res.redirect('http://localhost:3000/dashboard');
+    res.redirect('http://localhost:3000');
   }
 );
 // auth end
